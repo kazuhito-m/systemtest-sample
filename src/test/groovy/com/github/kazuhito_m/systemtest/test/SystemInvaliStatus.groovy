84028c07a1aa87b886a84ee72afbe1b15ea30b7f
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-03-29 15:28:46 +0900

    一つめのシナリオテストのクラスを作成。

---
+++ src/test/groovy/com/github/kazuhito_m/systemtest/test/SystemInvaliStatus.groovy
@@ -0,0 +1,18 @@
+package com.github.kazuhito_m.systemtest.test
+
+import com.github.kazuhito_m.systemtest.page.HeroPage
+import geb.spock.GebReportingSpec
+
+/**
+ * Created by kazuhito_m on 15/03/29.
+ */
+class SystemInvaliStatus extends GebReportingSpec {
+
+    def "Adminユーザが一人の場合に削除しようとしても削除できない"() {
+
+        given: "Adiminユーザが一人の場合"
+        when: "Adminユーザを削除しようとする"
+        then: "削除しようとしたユーザが削除できない"
+
+    }
+}
